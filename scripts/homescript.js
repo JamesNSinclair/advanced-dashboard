c01f6ea8ea0ba8187c1aa9e7dedb47bcec10157d
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2020-07-23 17:56:32 +0100

    Updates to trophy and css color touches

--- scripts/homescript.js
+++ scripts/homescript.js
@@ -1,6 +1,8 @@
 const log = document.getElementById('login')
 const reg = document.getElementById('register')
 const btn = document.getElementById('homebtn')
+const logbtn = document.getElementById('log')
+const regbtn = document.getElementById('reg')
 
 
 
@@ -8,10 +10,14 @@ function register() {
   log.style.left="-400px";
     reg.style.left="50px";
     btn.style.left = "110px";
+    regbtn.style.color = 'white';
+      logbtn.style.color = 'black';
 }
 
 function login() {
   log.style.left="";
     reg.style.left="";
     btn.style.left = "";
+    regbtn.style.color = 'black';
+    logbtn.style.color = 'white';
 }
